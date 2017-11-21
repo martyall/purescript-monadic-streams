e1317eba760b9fcef7af14c8cb63470368da86e4
Blinky <blinky3713@gmail.com>
2017-11-20 23:13:27 -0800

    first test

--- src/Data/Stream.purs
+++ src/Data/Stream.purs
@@ -1,7 +1,54 @@
 module Data.Stream where
 
-data Stream a = Stream forall s . {step :: s -> Step a s, initalState :: s}
+import Prelude
+
+import Data.Array (uncons, (:))
+import Data.Exists (Exists, mkExists, runExists)
+import Data.Maybe (Maybe(..))
+
+data StreamF a s = StreamF {step :: s -> Step a s, initialState :: s}
 
 data Step a s = Yield a s
               | Skip    s
               | Done
+
+data Stream a = Stream (Exists (StreamF a))
+
+instance functorStream :: Functor Stream where
+  map = mapStream
+
+stream :: forall a . Array a -> Stream a
+stream as = Stream <<< mkExists $
+    StreamF { step: next
+            , initialState: as
+            }
+  where
+    next :: Array a -> Step a (Array a)
+    next as' = case uncons as' of
+      Nothing -> Done
+      Just {head, tail} -> Yield head tail
+
+unstream :: forall a . Stream a -> Array a
+unstream as = runExists unstream' (unpackStream as)
+  where
+    unstream' :: forall s . StreamF a s -> Array a
+    unstream' (StreamF stream) = case stream.step stream.initialState of
+      Done -> []
+      Skip s' -> unstream' $ StreamF stream {initialState = s'}
+      Yield a s' -> a : unstream' (StreamF stream {initialState = s'})
+
+--------------------------------------------------------------------------------
+
+unpackStream :: forall a . Stream a -> Exists (StreamF a)
+unpackStream (Stream a) = a
+
+mapStream :: forall a b . (a -> b) -> Stream a -> Stream b
+mapStream f as = runExists mapStream' $ unpackStream as
+  where
+    mapStream' :: forall s. StreamF a s -> Stream b
+    mapStream' (StreamF stream) = Stream <<< mkExists $
+      StreamF stream { step = \s -> case stream.step s of
+                         Done -> Done
+                         Skip s' -> Skip s'
+                         Yield la s' -> Yield (f la) s'
+                     }
--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,21 @@
 module Test.Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "You should add some tests."
+import Control.Monad.Eff (Eff)
+import Data.Stream (stream, unstream)
+import Test.Spec (Spec, describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (RunnerEffects, run)
+
+main :: Eff (RunnerEffects ()) Unit
+main = run [consoleReporter] $ do
+  arraySpec
+
+arraySpec :: forall r . Spec r Unit
+
+arraySpec = describe "converting to and from arrays" do
+  it "unstream <<< stream == id" do
+    let testArray = [1,2,3]
+    testArray `shouldEqual` (unstream <<< stream $ testArray)
